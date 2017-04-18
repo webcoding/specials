1b258fb9ac76db5fa7c191be8bf299e619a6f1a7
jack <1395093509@qq.com>
2017-04-18 22:03:40 +0800

    更新

--- client/src/pages/support.vue
+++ client/src/pages/support.vue
@@ -31,9 +31,5 @@
       </a>
       <p>感谢 vue 的开发，没有 vue 生态圈，devnode 就将不完美</p>
     </div>
-    <div class="guide-links">
-      <span>← <a href="prev">上一页</a></a></span>
-      <span style="float:right"><a href="next">下一页</a></a> →</span>
-    </div>
   </div>
 </template>
