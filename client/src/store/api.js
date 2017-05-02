0c2f07e797e44c790b555bc5227c21b513a8b811
jack <1395093509@qq.com>
2017-05-02 10:16:37 +0800

    调整 api

--- client/src/store/api.js
+++ client/src/store/api.js
@@ -1,7 +1,35 @@
 
-import fetchApi from '@common/services/fetch-api'
+import axios from 'axios'
+// import fetchApi from '@common/services/fetch-api'
 import topicskusinfo from './mock/topicskusinfo'
 import topicskulist from './mock/topicskulist'
+
+// 添加请求拦截器
+axios.interceptors.request.use((config) => {
+  // 在发送请求之前做某事
+  return config
+}, function (error) {
+  // 请求错误时做些事
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use((response) => {
+  // 对响应数据做些事
+  return response
+}, function (error) {
+  // 请求错误时做些事
+  return Promise.reject(error)
+})
+
+// // 发送一个 get 请求
+// axios.get('package.json')
+//     .then((res) => {
+//       console.log(res)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
 
 /**
  * 创建临时数据
@@ -18,37 +46,54 @@ const setPromise = data => {
 //   },
 // }
 
+var ajax
 // 编译环境使用真实数据
 var getTopicInfo
 var getTopicList
 if (process.env.NODE_ENV !== 'development') {
   console.log('开发环境使用 fake 数据')
 
+  ajax = axios.create({
+    baseURL: 'http://api.cloudai.net/bookmark',
+    timeout: 1000,
+    headers: {
+      'X-Custom-Header': 'foobar',
+    },
+  })
+
   getTopicInfo = () => setPromise(topicskusinfo)
   getTopicList = () => setPromise(topicskulist)
 } else {
   console.log('编译环境使用真实数据')
+  ajax = axios.create({
+    baseURL: 'http://api.cloudai.net/bookmark',
+    timeout: 1000,
+    withCredentials: true,
+    headers: {
+      'X-Custom-Header': 'foobar',
+    },
+  })
 
   getTopicInfo = function (params) {
-    return fetchApi.get('/product/topicskusinfo', params)
+    return ajax.get('/product/topicskusinfo', params)
   }
   getTopicList = function (params) {
-    return fetchApi.get('/product/topicskulist', params)
+    return ajax.get('/product/topicskulist', params)
   }
   var getAboutMe = function (params) {
-    return fetchApi.get('me')
+    return ajax.get('me')
   }
   var getPost = function (id) {
-    return fetchApi.get(`articles/${id}`)
+    return ajax.get(`articles/${id}`)
   }
   var getPostList = function (params) {
-    return fetchApi.get('articles', params)
+    return ajax.get('articles', params)
   }
   var getPostListWithTag = function (tagId) {
-    return fetchApi.get('articles', { tag: tagId })
+    return ajax.get('articles', { tag: tagId })
   }
   var getAllTags = function (params) {
-    return fetchApi.get('tags')
+    return ajax.get('tags')
   }
 }
 
